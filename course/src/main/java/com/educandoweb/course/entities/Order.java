bf2458842cf279a5cedcbd988d2a81fc1926d367
Ricardo <ricardomedeiros116@gmail.com>
2026-01-08 14:35:40 -0300

    Order, Instant, ISO 8601

---
+++ course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -0,0 +1,60 @@
+package com.educandoweb.course.entities;
+
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table (name = "tb_order")
+public class Order implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order(){
+
+    }
+
+    public Order(Long id, Instant moment) {
+        this.id = id;
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
