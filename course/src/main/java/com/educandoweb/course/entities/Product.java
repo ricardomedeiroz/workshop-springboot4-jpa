f3b96014b08a46578f2f7f5616f79ba9a8a9ca2e
Ricardo <ricardomedeiros116@gmail.com>
2026-01-23 16:04:48 -0300

    Product-OrderItem one-to-many association

--- course/src/main/java/com/educandoweb/course/entities/Product.java
+++ course/src/main/java/com/educandoweb/course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -27,6 +28,10 @@ public class Product implements Serializable {
 
 
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
 
     public Product() {
     }
@@ -81,6 +86,14 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+    @JsonIgnore
+    public Set<Order> getOrders(){
+      Set<Order> set = new HashSet<>();
+      for (OrderItem x : items){
+          set.add(x.getOrder());
+      }
+      return set;
     }
 
     @Override
